fe1278da34652fc26d611a7af46811e1019654a4
Eseta Galea'i <esetag@uw.edu>
2018-05-11 19:25:03 -0700

    create shiny app with panels

--- exercise-3/server.R
+++ exercise-3/server.R
@@ -3,15 +3,25 @@
 library(shiny)
 library(ggplot2)
 # Create a shiny server that creates a scatterplot. 
+server <- function(input, output) {
+  
 
 # It should take as an input the x_var, y_var, color, and size
 # Save the result of `renderPlot` to output$scatter
-
+  output$scatter <- renderPlot({
     # Store x and y values to plot
-
+    x_values <- mpg[[input$x_var]]
+    y_values <- mpg[[input$y_var]]
 
     # Store the title of the graph in a variable
-
+    title_graph <- paste0("MPG Dataset: ", input$x_var, "v.s. ", input$y_var)
     
     # Create ggplot scatter
-
+    scatterplot <- ggplot(mpg) +
+        geom_point(mapping = aes(x = x_values, y = y_values),
+                   color = input$color, size = input$size) +
+        labs(title = title_graph, x = input$x_var, y = input$y_var)
+    scatterplot
+    })
+}
+shinyServer(server)
--- exercise-3/ui.R
+++ exercise-3/ui.R
@@ -1,25 +1,34 @@
 # UI for scatterplot
 library(shiny)
+library(ggplot2)
 
 # Get a vector of column names (from `mpg`) to use as select inputs
 select_values <- colnames(mpg)
 
 # Create a shinyUI with a `fluidPage` layout
-
-  # A page header
-
+ui <- fluidPage(
+  titlePanel("Participation 6: MPG Dataset Exploration"),
+  sidebarLayout(
+    sidebarPanel(
+      # A page header
+      #h1("Participation 6: MPG Dataset Exploration"),
   
-  # Add a select input for the x variable
-
+      # Add a select input for the x variable
+      selectInput("x_var", "X Variable", choices = select_values),
   
-  # Add a select input for the y variable
-
-
-  # Add a sliderInput to set the size of each point
-
-
-  # Add a selectInput that allows you to select a color from a list of choices
-
-
-
-  # Plot the output with the name "scatter"
+      # Add a select input for the y variable
+      selectInput("y_var", "Y Variable", choices = select_values),
+  
+      # Add a sliderInput to set the size of each point
+      sliderInput("size", "Size of point", 1, 10, value = 1, step = 1),
+  
+      # Add a selectInput that allows you to select a color from a list of choices
+      selectInput("color", "Color", choices = c("red", "blue", "yellow"))
+    ),
+    mainPanel(
+      # Plot the output with the name "scatter"
+      plotOutput("scatter")
+    )
+  )
+)
+shinyUI(ui)
